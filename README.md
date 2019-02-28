a3b569e16cb692081361f19e0820516085eb90d6
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-02-28 15:52:29 -0500

    asdfsdfadsfasdf

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SystemReady
+1. pythonReady.bat
+2. Install CUDA 9.0 & cudnn
+3. nnReady.bat
---
+++ pythonReady.bat
@@ -0,0 +1,12 @@
+pip install numpy
+pip install scipy
+pip install pandas
+pip install matplotlib
+pip install scikit-learn
+pip install pathlib
+pip install pypng
+pip install pillow
+pip install django
+pip install image
+pip install opencv-python opencv-contrib-python
+
