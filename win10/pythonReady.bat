149adf57eba4c0a14025f9c73649211207bec662
Elliot <hylee101001@gmail.com>
2019-11-07 14:59:04 -0800

    aaaa

--- win10/pythonReady.bat
+++ win10/pythonReady.bat
@@ -10,3 +10,17 @@ pip install django
 pip install image
 pip install opencv-python opencv-contrib-python
 
+
+py -2 -m install numpy
+py -2 -m install scipy
+py -2 -m install pandas
+py -2 -m install matplotlib
+py -2 -m install scikit-learn
+py -2 -m install pathlib
+py -2 -m install pypng
+py -2 -m install pillow
+py -2 -m install django
+py -2 -m install image
+py -2 -m install opencv-python opencv-contrib-python
+
+
